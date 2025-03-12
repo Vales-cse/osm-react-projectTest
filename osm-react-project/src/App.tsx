750dd07febbd00a2221b2fc3911e06184c41cf45
Valerio Ascione <99ascione@gmail.com>
2025-03-12 16:51:26 +0100

    Initial setup to search location

--- osm-react-project/src/App.tsx
+++ osm-react-project/src/App.tsx
@@ -4,13 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Map from "./components/Map";
 import LocationSearch from "./components/LocationSearch";
 import type { Place } from "./api/Place";
+import { useState } from "react";
 
 function App() {
+
+  const [place, setPlace] = useState<Place | null>(null);
+
   return (
     <div className="App">
       <div className="row">
-        <div className="col-4"><LocationSearch/></div>
-        <div className="col-8"><Map/></div>
+        <div className="col-4">
+          <LocationSearch onPlaceClick={(place) => setPlace(place)}/>
+        </div>
+        <div className="col-8"><Map place={place}/></div>
       </div>
     </div>
   );
--- osm-react-project/src/components/LocationSearch.tsx
+++ osm-react-project/src/components/LocationSearch.tsx
@@ -1,5 +1,32 @@
-import type { Place } from "../api/Place"
+import type { Place } from "../api/Place";
+import React, { useState } from "react";
 
-export default function LocationSearch() {
-    return <div>Search</div>
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [term, setTerm] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('need search API', term);
+  }
+
+  return (
+    <div className="mb-3">
+      <form onSubmit={handleSubmit}>
+        <label className="form-label">Location search:</label>
+        <input
+          type="location"
+          className="form-control"
+          id="location"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
