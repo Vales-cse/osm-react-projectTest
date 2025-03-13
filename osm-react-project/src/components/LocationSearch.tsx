509feda1feac2e84375183e5d85c78d3f0c69a2a
Valerio Ascione <99ascione@gmail.com>
2025-03-13 10:01:39 +0100

    Implemented OSM api and fixed LocationSearch behavior
    Implemented the OSM api to retrive the data correctly and added all the searching/deleting logic to the LocationSearch component.
    Next step: implement Map visualization

--- osm-react-project/src/components/LocationSearch.tsx
+++ osm-react-project/src/components/LocationSearch.tsx
@@ -1,5 +1,6 @@
 import type { Place } from "../api/Place";
 import React, { useState } from "react";
+import { search } from "../api/search";
 
 interface LocationSearchProps {
   onPlaceClick: (place: Place) => void;
@@ -12,8 +13,35 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    console.log('need search API', term);
-  }
+    const results = await search(term);
+    setPlaces([...places, ...results]);
+  };
+
+  const handleDelete = (id: number, event: React.MouseEvent<HTMLButtonElement>) => {
+    //Evito di attivare onClick della lista (oggetto parent)
+    event?.stopPropagation();
+    setPlaces(places.filter(place => place.id !== id));
+  };
+
+  const itemSearched = () => {
+    return (
+      <>
+        <h1 className="fs-5">Results:</h1>
+        <ul className="list-group">
+          {places.map((place) => (
+            <li
+              key={place.id}
+              className="list-group-item list-group-item-action d-flex justify-content-between"
+              onClick={() => onPlaceClick(place)}
+            >
+              {place.name}
+              <button className="btn btn-danger" onClick={(event) => handleDelete(place.id, event)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  };
 
   return (
     <div className="mb-3">
@@ -27,6 +55,8 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
           onChange={(e) => setTerm(e.target.value)}
         />
       </form>
+      <h1 className="text-uppercase fs-5">Found Location:</h1>
+      {itemSearched()}
     </div>
   );
 }
